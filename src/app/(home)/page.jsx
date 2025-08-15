67e56e9119f6f706f21e8b451e3428737fc8ed36
nguyendat <nguyendat08022004@gmail.com>
2025-08-15 16:55:16 +0700

    done

--- src/app/(home)/page.jsx
+++ src/app/(home)/page.jsx
@@ -3,7 +3,7 @@ import { HomeView } from 'src/sections/home/view';
 // ----------------------------------------------------------------------
 
 export const metadata = {
-  title: 'Minimals UI: The starting point for your next project',
+  title: 'DrMayday',
   description:
     'The starting point for your next project with Minimal UI Kit, built on the newest version of Material-UI ©, ready to be customized to your style',
 };
