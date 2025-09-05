c4bad4377b903011ff3b2d5f31c7700c9f6061d5
jasjjds <jasjjds>
2025-09-06 00:49:57 +0700

    hello

--- src/layouts/nav-config-dashboard.jsx
+++ src/layouts/nav-config-dashboard.jsx
@@ -53,9 +53,10 @@ export function useNavData() {
         ...(user?.role === 1
           ? [
             { title: 'Quản lý bệnh án', path: paths.dashboard.MedicalRecords.record},
-             
-              { title: 'Bệnh án chờ xử lý', path: paths.dashboard.MedicalRecords.pending },
-              { title: 'Bệnh án đang xử lý', path: paths.dashboard.MedicalRecords.processing },
+            { title: 'Bệnh án chờ xử lý', path: paths.dashboard.MedicalRecords.pending },
+            { title: 'Bệnh án đang xử lý', path: paths.dashboard.MedicalRecords.processing },
+            { title: 'Quản lý bệnh án demo', path: paths.dashboard.medicalRecordStaff.root },
+            { title: 'Tạo bệnh án demo', path: paths.dashboard.medicalRecordStaff.create},
             
           ]
           : [{ title: 'Tạo mẫu bệnh án', path: paths.dashboard.MedicalRecords.create }]),
