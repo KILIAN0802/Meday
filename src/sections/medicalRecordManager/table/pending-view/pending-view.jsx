0b5d4441039e79a7a3492a9190fad2803f2cbd4c
nguyendat <nguyendat08022004@gmail.com>
2025-09-07 16:45:35 +0700

    change1

--- src/sections/medicalRecordManager/table/pending-view/pending-view.jsx
+++ src/sections/medicalRecordManager/table/pending-view/pending-view.jsx
@@ -1,1 +1,9 @@
-export * from './pending-view'
+'use client';
+
+import { PendingMedicalRecords } from '../table-pending'
+import { getAppointmentID } from 'src/api/appointments-staff';
+
+export function PendingView(){
+    
+    return <PendingMedicalRecords/>;
+}
